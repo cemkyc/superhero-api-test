a52b353674fb079b44d5cb351f03c61807385cb0
cemkyc <saint.maximus1@gmail.com>
2026-05-31 13:27:40 +0300

    Hot fix Добавил комментарии в коде

--- gettallesthero.py
+++ gettallesthero.py
@@ -6,11 +6,13 @@ HeroPath = Path(__file__).resolve().parent / "api" / "all.json"
 with open(HeroPath, 'r', encoding='utf-8' ) as f:
     Heroes= json.load(f)
 
+# проверяем есть ли работа
 def hasWork(hero):
     occup = hero["work"]["occupation"]
     workStatus = (occup != "-" and bool(occup.strip()))
     return workStatus
 
+# приводим все велечины роста к одной системе исчисления
 def getHeightHaveSameCount(hero):
     height = hero["appearance"]["height"][1].split()
     if "cm" in height:
@@ -20,6 +22,7 @@ def getHeightHaveSameCount(hero):
     # print(hero["name"], "Wrong parametr")
     return 0
 
+# НАходим высокого самого героя
 def getTallestHero(gender, workStatus):
     heroesFiltered = []
     for hero in Heroes:
